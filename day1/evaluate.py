d33e689426a0040a5022336b0ae3441f6151c763
deflatedlatte <deflatedlatte@gmail.com>
2024-06-18 00:45:58 +0900

    Add gsm8k_cot_complex testcase

--- day1/evaluate.py
+++ day1/evaluate.py
@@ -1,3 +1,5 @@
+import sys
+
 from tenacity import retry, wait_chain, wait_fixed
 
 import backends.openai
@@ -8,9 +10,34 @@ import testcases.gsm8k
 def prompt_openai_messages_with_retries(*args, **kwargs):
     return backends.openai.prompt_messages(*args, **kwargs)
 
-r = run_test(
-    testcases.gsm8k.get_test_dataset,
-    testcases.gsm8k.preprocess_data,
-    testcases.gsm8k.do_test,
-    prompt_openai_messages_with_retries
-)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: {} <testname>".format(sys.argv[0]))
+        sys.exit(1)
+
+    test_suites = {
+        "gsm8k": (
+            testcases.gsm8k.get_test_dataset,
+            testcases.gsm8k.preprocess_data,
+            testcases.gsm8k.do_test_question_only,
+        ),
+        "gsm8k_cot_complex": (
+            testcases.gsm8k.get_test_dataset,
+            testcases.gsm8k.preprocess_data,
+            testcases.gsm8k.do_test_cot_complex_prompt,
+        )
+    }
+    test_suite = test_suites.get(sys.argv[1], None)
+
+    if test_suite is None:
+        print("valid test names are: {}".format(",".join(test_suites)))
+        sys.exit(1)
+
+    dataset_loader, data_preprocessor, test_backend = test_suite
+
+    test_results = run_test(
+        dataset_loader,
+        data_preprocessor,
+        test_backend,
+        prompt_openai_messages_with_retries
+    )
--- day1/testcases/gsm8k.py
+++ day1/testcases/gsm8k.py
@@ -1,5 +1,9 @@
+import requests
+
 from backends import SystemPrompt, UserPrompt
 from datasets import load_dataset
+
+_cot_hub_cache = {}
 
 def get_test_dataset():
     gsm8k = load_dataset('gsm8k', 'main')
@@ -9,11 +13,35 @@ def get_test_dataset():
 def preprocess_data(dataset):
     return dataset
 
-def do_test(testcase, prompt_backend):
+def do_test_question_only(testcase, prompt_backend):
     q, a = testcase['question'], testcase['answer']
     messages = [
-        SystemPrompt("Follow the given examples and answer the question."),
+        SystemPrompt("Answer the following question."),
         UserPrompt(f"Q: {q}")
     ]
     result = prompt_backend(messages)
-    return f"A_model: {result}\nA: {a}\n--------\n"
+    return {
+        "question": q,
+        "answer": a,
+        "answer_model": result
+    }
+
+def do_test_cot_complex_prompt(testcase, prompt_backend):
+    prompt_base = _cot_hub_cache.get("complex_prompt")
+    if prompt_base is None:
+        prompt_base = requests.get(
+            "https://raw.githubusercontent.com/FranxYao/chain-of-thought-hub/"
+            "main/gsm8k/lib_prompt/prompt_hardest.txt"
+        ).text
+        _cot_hub_cache["complex_prompt"] = prompt_base
+    q, a = testcase['question'], testcase['answer']
+    messages = [
+        SystemPrompt("Follow the given examples and answer the question."),
+        UserPrompt(f"{prompt_base}\n\nQuestion: {q}\n")
+    ]
+    result = prompt_backend(messages)
+    return {
+        "question": q,
+        "answer": a,
+        "answer_model": result
+    }
