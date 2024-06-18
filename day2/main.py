feec6dd9c60081ecb33467c027e511d32a6403df
deflatedlatte <deflatedlatte@gmail.com>
2024-06-19 05:29:43 +0900

    Make the program fail when all retrievals are irrelevant

--- day2/main.py
+++ day2/main.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import configparser
 
@@ -8,10 +9,17 @@ from pipelines import (
 
 def main():
     db = prepare_db.run()
+    #user_query = "i love kimchi"    # use this query to test if 'hits' become empty
     user_query = "agent memory"
     docs = db.similarity_search(user_query)
+    hits = []
     for doc in docs:
-        print(determine_relevance.run(user_query, doc))
+        response = determine_relevance.run(user_query, doc)
+        if response.strip() == "isContextAppropriate: yes":
+            hits.append(doc)
+    if not hits:
+        print("Sorry, I couldn't find any documents relevant to your query.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
